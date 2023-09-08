bdfb15ad762cda3c20e903cde7a5880eb1782377
ChineduG <investorgbriel@gmail.com>
2023-09-08 14:18:46 +0100

    Added Ejs template engine

--- routes/shop.js
+++ routes/shop.js
@@ -1,25 +1,22 @@
 const path = require('path');
+
 const express = require('express');
 
 // const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
-
-
-//! This particualr code below threw an error
-// const path = require('path');
-// const express = require('express');
-// const adminData = require('./admin');
-// const router = express.Router();
-// router.get('/', (req, res, next) => {
-//     console.log(adminData.route);
-//     // console.log('shop.js', adminData.products);
-//     res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
-// });
-// module.exports = router;
